af095dfdd7cf8de46addabe9b6d8db17c7a93047
sherman-1 <herman.simon.lm@gmail.com>
2023-03-21 09:36:17 +0100

    run

--- run_OF.sh
+++ run_OF.sh
@@ -1,11 +1,17 @@
 #!/bin/sh 
 
 
+if [ ! -d "res_bench" ]
+then 
+    mkdir res_bench 
+fi
 
+if [ -d "temp" ]
+then
+    rm -rf temp
+fi
 
 abs_path=$(pwd)/res_bench
-
-
 
 for dir in $(find Modified_data/ -maxdepth 4 -mindepth 4); do
 
@@ -16,9 +22,12 @@ for dir in $(find Modified_data/ -maxdepth 4 -mindepth 4); do
     length=$(echo  ${dir} | awk 'BEGIN { FS = "/" }; { print $5 }')
 
     
-    orthofinder -f Modified_data/$cond/$seq/$action/$length -og -o temp
+    orthofinder -f Modified_data/$cond/$seq/$action/$length -op -o temp
 
-    
+    mv temp/Results_Mar21/Log.txt $abs_path/${cond}_${seq}_${action}_${length}
+
+    rm -rf temp
+
 
     # grep -Eo "[0-9]+"
 
