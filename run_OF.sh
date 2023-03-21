2b54cb1ab86cbb3472664564749854f21985daf9
sherman-1 <herman.simon.lm@gmail.com>
2023-03-21 17:46:04 +0100

    v0.0.2

--- run_OF.sh
+++ run_OF.sh
@@ -11,7 +11,7 @@ then
     mkdir temp
 fi
 
-abs_path=$(pwd)/res_bench
+res_bench
 
 i=1
 
@@ -27,18 +27,18 @@ for dir in $(find Modified_data/ -maxdepth 4 -mindepth 4); do
     # Compute orthologs with OrthoFinder
     orthofinder -t 16 -f Modified_data/$cond/$seq/$action/$length -o ${cond}_${seq}_${action}_${length}
 
-    mkdir ${abs_path}/${cond}_${seq}_${action}_${length}
-    mkdir ${abs_path}/${cond}_${seq}_${action}_${length}/GeneCount
+    mkdir res_bench/${cond}_${seq}_${action}_${length}
+    mkdir res_bench/${cond}_${seq}_${action}_${length}/GeneCount
 
     # Use OrthoFinder tools to count how many genes per species for each Phylogenetic Orthogroup of node N0 ( enclude all species )
     python3 ~/utils/OrthoFinder/tools/orthogroup_gene_count.py ${cond}_${seq}_${action}_${length}/*/Phylogenetic_Hierarchical_Orthogroups/N0.tsv 
 
-    mv ${cond}_${seq}_${action}_${length}/*/Phylogenetic_Hierarchical_Orthogroups/N0.GeneCount.csv abs_path/${cond}_${seq}_${action}_${length}/GeneCount
+    mv ${cond}_${seq}_${action}_${length}/*/Phylogenetic_Hierarchical_Orthogroups/N0.GeneCount.csv res_bench/${cond}_${seq}_${action}_${length}/GeneCount
 
-    python3 ~/utils/OrthoFinder/tools/create_files_for_hogs.py ${cond}_${seq}_${action}_${length}/* abs_path/${cond}_${seq}_${action}_${length}/HOGS_fa N0
+    python3 ~/utils/OrthoFinder/tools/create_files_for_hogs.py ${cond}_${seq}_${action}_${length}/* res_bench/${cond}_${seq}_${action}_${length}/HOGS_fa N0
 
     rm -rf ${cond}_${seq}_${action}_${length}
 
-    break
+    
 
 done
