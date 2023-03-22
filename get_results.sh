df0818333756a08de350bb519f1cc612cb8791e2
sherman-1 <herman.simon.lm@gmail.com>
2023-03-22 15:21:26 +0100

    Search for results

--- get_results.sh
+++ get_results.sh
@@ -1,8 +1,15 @@
 #!/bin/sh
 
 
-for file in res_bench/*; do
+# Store which are the Orthologs of the 9 sequences used for benchmark
 
-    echo $file
+for file in rna*.txt; do
+
+    cond=$(grep -Po "\\d+" $file)
+
+    for id in $(cat $file); do
+
+        grep -r $id Single_Hog
 
 done
+
